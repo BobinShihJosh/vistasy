a32f2cad30da36d3f1c3bcc3a02526b7a92e3a0e
BobinShihJosh <bos2@uw.edu>
2024-02-27 23:58:20 -0800

    change name

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading text-gray-700">
-          Unleash Your Creative Vision with Vistasy
+          Unleash Your Creative Vision with IntelliPic
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
